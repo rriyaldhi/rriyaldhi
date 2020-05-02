a83ec2e28be7f6897aa3205f64253ecee95f3bad
rriyaldhi <rriyaldhi@gmail.com>
2020-05-02 15:01:52 +0200

    fix production build

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -14,8 +14,8 @@ module.exports = {
 
   entry: './src/index.tsx',
   output: {
-    path: __dirname + '/dist',
-    filename: 'main.js'
+    filename: '[name]-[chunkhash].js',
+    chunkFilename: '[name]-[chunkhash].js'
   },
   plugins: [
     new webpack.ProgressPlugin(),
@@ -23,9 +23,26 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './src/index.ejs'
     }),
-    new JavaScriptObfuscator ()
+    new JavaScriptObfuscator()
   ],
 
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      chunks: 'all',
+      maxInitialRequests: Infinity,
+      minSize: 0,
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name(module) {
+            const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+            return `npm.${packageName.replace('@', '')}`;
+          },
+        },
+      },
+    },
+  },
   module: {
     rules: [
       {

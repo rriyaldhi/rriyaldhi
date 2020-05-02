a775b570767cb15085b5951ea0953ee0198095f3
rriyaldhi <rriyaldhi@gmail.com>
2020-05-02 22:47:32 +0200

    add scene singleton

---
+++ src/utility/scene-singleton/SceneSingleton.test.tsx
@@ -0,0 +1,24 @@
+import SceneSingleton from './SceneSingleton';
+import EngineSingleton from '../engine-singleton/EngineSingleton';
+import {Engine, Scene} from 'babylonjs';
+
+describe('SceneSingleton', () =>
+{
+  it('should get the same scene', () => {
+    const canvas: HTMLCanvasElement = document.createElement('canvas');
+    const engine: Engine = EngineSingleton.getInstance(canvas);
+    const scene1: Scene = SceneSingleton.getInstance(engine);
+    const scene2: Scene = SceneSingleton.getInstance(engine);
+    expect(scene1).toEqual(scene2);
+  });
+
+  it('should get the different scene', () => {
+    const canvas: HTMLCanvasElement = document.createElement('canvas');
+    const engine: Engine = EngineSingleton.getInstance(canvas);
+    const scene1: Scene = SceneSingleton.getInstance(engine);
+    const canvas2: HTMLCanvasElement = document.createElement('canvas');
+    const engine2: Engine = EngineSingleton.getInstance(canvas2);
+    const scene2: Scene = SceneSingleton.getInstance(engine2);
+    expect(scene1).not.toEqual(scene2);
+  });
+});
---
+++ src/utility/scene-singleton/SceneSingleton.tsx
@@ -0,0 +1,24 @@
+import {Color4, Engine, Scene} from 'babylonjs';
+
+export default class SceneSingleton
+{
+  private static _instanceMap: Map<Engine, Scene>;
+
+  private constructor() {}
+
+  public static getInstance(engine?: Engine): Scene
+  {
+    if (SceneSingleton._instanceMap == null || !SceneSingleton._instanceMap.has(engine))
+    {
+      const scene: Scene = new Scene(engine);
+      scene.clearColor = new Color4(1, 1, 1, 1);
+      if (SceneSingleton._instanceMap == null)
+      {
+        SceneSingleton._instanceMap = new Map<Engine, Scene>();
+      }
+      SceneSingleton._instanceMap.set(engine, scene);
+      return scene;
+    }
+    return SceneSingleton._instanceMap.get(engine);
+  }
+}
